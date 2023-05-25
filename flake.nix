6d9356bc463a0d0cc2983bafd0ba1ba75d0a7ae3
StarrFox <StarrFox6312@gmail.com>
2023-05-25 21:09:34 +0000

    feat(init): basic project structure

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk.url = "github:nix-community/naersk";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {self, flake-utils, naersk, nixpkgs}:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+        };
+
+        naersk' = pkgs.callPackage naersk {};
+        project_cargo = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+      in rec {
+        defaultPackage = naersk'.buildPackage {
+          name = "denotify";
+          src = ./.;
+          # extra built inputs here
+          buildInputs = with pkgs; [];
+          version = project_cargo.package.version;
+        };
+
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [ rustc cargo ];
+          buildInputs = with pkgs; [ direnv just commitizen ];
+        };
+      }
+    );
+}
